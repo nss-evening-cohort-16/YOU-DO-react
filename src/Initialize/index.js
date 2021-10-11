4ad69ca6676ee1fba33de83a6790c72a38f3e49d
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-11 14:07:58 -0500

    styled with styled-components

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { getTodos } from '../api/data/todoData';
 import Todo from '../components/Todo';
 import TodoForm from '../components/TodoForm';
+
+const Container = styled.div`
+  width: 60%;
+  margin: auto;
+  padding: 50px 0;
+
+  h1 {
+    color: white;
+    text-align: center;
+    font-size: 64px;
+    font-weight: 400;
+  }
+`;
 
 function Initialize() {
   const [todos, setTodos] = useState([]);
@@ -12,17 +26,20 @@ function Initialize() {
   }, []);
 
   return (
-    <>
+    <Container>
+      <h1>YOU-DO</h1>
       <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      {todos.map((todo) => (
-        <Todo
-          key={todo.firebaseKey}
-          taco={todo}
-          setTodos={setTodos}
-          setEditItem={setEditItem}
-        />
-      ))}
-    </>
+      <div className="mt-5">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.firebaseKey}
+            taco={todo}
+            setTodos={setTodos}
+            setEditItem={setEditItem}
+          />
+        ))}
+      </div>
+    </Container>
   );
 }
 
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -64,13 +64,13 @@ export default function TodoForm({ obj, setTodos, setEditItem }) {
             Name
           </label>
           <input
-            className="form-control form-control-lg me-3"
+            className="form-control form-control-lg me-1"
             type="text"
             id="name"
             name="name"
             value={formInput.name}
             onChange={handleChange}
-            placeholder="Enter A You Do!"
+            placeholder="ADD A YOU-DO"
             required
           />
           <button className="btn btn-success" type="submit">
