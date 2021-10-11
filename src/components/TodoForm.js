34010524eb3bf97c6e65d2e520a99909d4b0c5ff
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-11 08:26:31 -0500

    add notes

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createTodo } from '../api/data/todoData';
 
+// Create an initial state object so that it can be reused in the component
 const initialState = {
   name: '',
   complete: false,
   uid: '',
 };
 export default function TodoForm({ obj, setTodos }) {
+  // set the default state to the initialState object
   const [formInput, setFormInput] = useState(initialState);
 
+  // when the component mounts, check if a firebasekey exists. If it does, set the value of formInput to the obj values
   useEffect(() => {
     if (obj.firebaseKey) {
       setFormInput({
@@ -20,8 +23,10 @@ export default function TodoForm({ obj, setTodos }) {
         uid: obj.uid,
       });
     }
+    // rerender the component if the obj value is different
   }, [obj]);
 
+  // On call of the resetForm function, reset the state to the initialState
   const resetForm = () => {
     setFormInput({ ...initialState });
   };
@@ -33,6 +38,7 @@ export default function TodoForm({ obj, setTodos }) {
     }));
   };
 
+  // Since we are using this form for both creating and updating, we need to use logic to determine which method to run. If there is a firebaseKey, we know that we are updating.
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj.firebaseKey) {
@@ -48,10 +54,9 @@ export default function TodoForm({ obj, setTodos }) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Name
-          <br />
+        <div className="mb-3 d-flex">
           <input
+            className="form-control form-control-lg me-3"
             type="text"
             id="name"
             name="name"
@@ -60,8 +65,10 @@ export default function TodoForm({ obj, setTodos }) {
             placeholder="Enter A You Do!"
             required
           />
-        </label>
-        <button type="submit">Submit</button>
+          <button className="btn btn-success" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
