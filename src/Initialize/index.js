103795ccfa03dd02ac0bcad012663cac6542bf6a
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-13 15:39:48 -0500

    authentication without categories

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import styled from 'styled-components';
 import { getTodos } from '../api/data/todoData';
 import TodoForm from '../components/TodoForm';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 const Container = styled.div`
   width: 60%;
@@ -26,16 +29,43 @@ const Container = styled.div`
 function Initialize() {
   const [todos, setTodos] = useState([]);
   const [editItem, setEditItem] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getTodos().then(setTodos);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        // something to happen
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0],
+        };
+        setUser(userInfoObj);
+        // TODO: Refactor code for users to only see their todos
+        getTodos().then(setTodos);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
   }, []);
 
   return (
     <Container>
-      <Navigation />
-      <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
+      {user ? (
+        <>
+          <Navigation />
+          <h1>YOU DO</h1>
+          <TodoForm
+            obj={editItem}
+            setTodos={setTodos}
+            setEditItem={setEditItem}
+          />
+          <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
+        </>
+      ) : (
+        <SignIn user={user} />
+      )}
     </Container>
   );
 }
